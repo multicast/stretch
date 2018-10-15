65f0e5c5000207c5c1f6cde8dd3690ada60ff817
Matej Kovac <multicast@users.noreply.github.com>
2018-10-15 21:03:49 +0200

    run bash by default

--- build/sbin/entrypoint.sh
+++ build/sbin/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 BASE_DEBUG=${BASE_DEBUG:=0}
 BASE_NOEXIT=${BASE_NOEXIT:=0}
 
@@ -19,4 +18,23 @@ test ${BASE_DEBUG} -ge 1 && {
 test ${BASE_DEBUG} -ge 1 && {
   echo Running: \"$@\"
 }
-exec "$@"
+
+test $# -lt 1 && {
+  echo bash >> /etc/rc.local
+}
+test $# -eq 1 && {
+  echo $* >> /etc/rc.local
+}
+test $# -ge 2 && {
+  test "x$1" = "x/usr/bin/dumb-init" -a "x$2" = "x/etc/rc.local" && {
+    echo Running dumb-init with rc.local
+  } || {
+    echo $* >> /etc/rc.local
+  }
+}
+
+test ${BASE_DEBUG} -ge 2 && {
+  cat /etc/rc.local
+}
+
+exec /usr/bin/dumb-init /etc/rc.local
